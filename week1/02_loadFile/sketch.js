693ca380673c763f45908bf18ca052c3a8aec2d7
darokun <daloharodriguez@gmail.com>
2017-02-23 23:01:29 +0100

    finish second session: load text

--- week1/02_loadFile/sketch.js
+++ week1/02_loadFile/sketch.js
@@ -1,7 +1,37 @@
-function setup() {
+// var txt;
 
+function loadFile() {
+  loadStrings("rainbow.txt", fileLoaded);
+}
+
+function fileLoaded(data) {
+  // txt = data;
+  createP(join(data, "<br/>"));
+}
+
+function fileSelected(file) {
+  createP(file.name + " " + file.size + " " + file.type);
+
+  if (file.type == "text") {
+      createP(file.data);
+  } else {
+      createP("ERROR: I need a text file.");
+  }
+  // console.log(file);
+}
+
+
+function setup() {
+  noCanvas();
+
+  var button = select("#loadfile");
+  button.mousePressed(loadFile);
+
+  createFileInput(fileSelected);
+
+  // console.log(txt);
 }
 
 function draw() {
-  
+
 }
